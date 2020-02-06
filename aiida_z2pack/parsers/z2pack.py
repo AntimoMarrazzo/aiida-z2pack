f1a3c8a0f9144c607f25ed660bc3dc244c270c79
Crivella <davidecrivella@yahoo.it>
2020-02-06 10:23:54 +0100

    Fixed parser

--- aiida_z2pack/parsers/z2pack.py
+++ aiida_z2pack/parsers/z2pack.py
@@ -5,45 +5,28 @@ from aiida.parsers.parser import Parser
 from aiida.plugins import DataFactory, CalculationFactory
 
 Dict              = DataFactory('dict')
-KpointsData       = DataFactory('array.kpoints')
-BandsData         = DataFactory('array.bands')
-Z2packCalculation = CalculationFactory('z2pack')
+Z2packCalculation = CalculationFactory('z2pack.z2pack')
 
 
 class Z2packParser(Parser):
     """
     Z2packParser
     """    
-    _outarray_name = 'output_data'
-    
-
-    def __init__(self, calculation):
-        """
-        Initialize the instance of Z2pack parser
-        """
-        # check for valid input
-        if not isinstance(calculation, Z2packCalculation):
-            raise exceptions.OutputParsingError(
-                "Input must calc must be a Z2packCalculation")
-        super(Z2packParser, self).__init__(calculation)
-
     def parse(self, **kwargs):
         try:
             out_folder = self.retrieved
         except exceptions.NotExistent:
-            return self.exit(self.exit_codes.ERROR_NO_RETRIEVED_FOLDER)
+            return self.exit_codes.ERROR_NO_RETRIEVED_FOLDER
 
         # Checks for error output files
-        if self.node._ERROR_W90_FILE in out_folder.list_object_names():
-            return self.exit(self.exit_codes.ERROR_UNEXPECTED_FAILURE)
+        if Z2packCalculation._ERROR_W90_FILE in out_folder.list_object_names():
+            return self.exit_codes.ERROR_UNEXPECTED_FAILURE
 
         try:
-            filpath = out_folder.get_abs_path(
-                self.node._OUTPUT_RESULTS_FILE)
-            with open(filpath, 'r') as fil:
-                    data = json.load(fil)
+            with out_folder.open(Z2packCalculation._OUTPUT_RESULT_FILE) as fil:
+                data = json.load(fil)
         except OSError:
-            return self.exit(self.exit_codes.ERROR_MISSING_RESULTS_FILE)
+            return self.exit_codes.ERROR_MISSING_RESULTS_FILE
 
         gap_f   = len(data['convergence_report']['GapCheck']['FAILED'])
         move_f  = len(data['convergence_report']['MoveCheck']['FAILED'])        
@@ -54,11 +37,10 @@ class Z2packParser(Parser):
         data['Tests_passed'] = success
         
         try:
-            filpath = out_folder.get_abs_path(self.node._OUTPUT_Z2PACK_FILE)
-            with open(filpath, 'r') as fil:
-                    out_file = fil.readlines()
+            with out_folder.open(Z2packCalculation._OUTPUT_Z2PACK_FILE) as fil:
+                out_file = fil.readlines()
         except OSError:
-            return self.exit(self.exit_codes.ERROR_MISSING_Z2PACK_OUTFILE)
+            return self.exit_codes.ERROR_MISSING_Z2PACK_OUTFILE
 
         #out_file = out_file.split("\n")
         out_file = [i.strip('\n') for i in out_file]
