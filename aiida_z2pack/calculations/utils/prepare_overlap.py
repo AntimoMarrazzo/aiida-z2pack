a7e52b0a6ae31d8c59747588737ea9ad90987b7f
Crivella <davidecrivella@yahoo.it>
2020-02-14 10:10:35 +0100

    Adapted to changes to method names in QE plugin.

--- aiida_z2pack/calculations/utils/prepare_overlap.py
+++ aiida_z2pack/calculations/utils/prepare_overlap.py
@@ -1,16 +1,18 @@
 from aiida_quantumespresso.calculations.namelists import NamelistsCalculation
 
+class Temp(NamelistsCalculation):
+	pass
+
 
 def prepare_overlap(cls,folder):
 	parameters = cls.inputs.overlap_parameters.get_dict()
 	blocked    = cls._blocked_keywords_overlap
-	# namelists  = NamelistsCalculation._default_namelists
 
-	NamelistsCalculation._blocked_keywords = blocked
+	Temp._blocked_keywords = blocked
 
-	parameters = NamelistsCalculation._set_blocked_keywords(parameters)
+	parameters = Temp.set_blocked_keywords(parameters)
 
-	content = NamelistsCalculation._generate_input_file(parameters) + '\n\n'
+	content = Temp.generate_input_file(parameters) + '\n\n'
 
 	input_filename = cls._INPUT_OVERLAP_FILE
 
